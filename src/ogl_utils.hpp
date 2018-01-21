20039ffd48687b8c838de50585d2cb2b12479114
mhsjlw <mhsjlw@users.noreply.github.com>
2018-01-20 21:21:47 -0500

    Fix image loading

--- src/ogl_utils.hpp
+++ src/ogl_utils.hpp
@@ -1,6 +1,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 
 #include <iostream>
+#include <string.h>
 #include "stb_image.h"
 #include "glad/glad.h"
 
@@ -42,6 +43,7 @@ int customCompileShader(GLenum shaderType, const char *file, unsigned int *shade
 unsigned int load2DTexture(const char* file) {
  int width, height, nrChannels;
  unsigned int texture;
+ stbi_set_flip_vertically_on_load(1);
  unsigned char *data = stbi_load(file, &width, &height, &nrChannels, 0);
  if (data) {
   glGenTextures(1, &texture);
@@ -52,7 +54,7 @@ unsigned int load2DTexture(const char* file) {
   //set texture filtering parameters
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
   glGenerateMipmap(GL_TEXTURE_2D);
  } else {
   std::cerr << "Failed to load texture '" << file << "'!" << std::endl;
